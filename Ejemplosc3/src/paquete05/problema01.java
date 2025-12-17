077533cf794216f4431790cf44f815f320212542
jhon pardo <jhonedupardo15@gmail.com>
2025-12-17 12:38:41 -0500

    paquete 05- primero cambio

--- Ejemplosc3/src/paquete05/problema01.java
+++ Ejemplosc3/src/paquete05/problema01.java
@@ -3,11 +3,35 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package paquete05;
-
+import java.util.Scanner;
 /**
  *
  * @author USUARIO
  */
 public class problema01 {
+
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+
+        String[] nombresEstaciones = new String[5];
+        String[] encargados = new String[5];
+        double[][] produccion = new double[5][12];
+        double[] totalProduccion = new double[5];
+
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Ingrese nombre de la estación");
+            nombresEstaciones[i] = entrada.nextLine();
+
+            System.out.println("Ingrese nombre del encargado:");
+            encargados[i] = entrada.nextLine();
+
+            for (int j = 0; j < 12; j++) {
+                System.out.println("Producción del mes");
+                produccion[i][j] = entrada.nextDouble();
+                
+            }
+            }
+        }
+    }
     
-}
+            
