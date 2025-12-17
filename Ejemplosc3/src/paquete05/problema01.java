d4539002fdd3e1a5549cf4439b94dcc925589bfd
jhon pardo <jhonedupardo15@gmail.com>
2025-12-17 12:59:27 -0500

    paquete 05- segundo cambio

--- Ejemplosc3/src/paquete05/problema01.java
+++ Ejemplosc3/src/paquete05/problema01.java
@@ -8,7 +8,7 @@ import java.util.Scanner;
  *
  * @author USUARIO
  */
-public class problema01 {
+public class Problema01 {
 
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
@@ -16,21 +16,39 @@ public class problema01 {
         String[] nombresEstaciones = new String[5];
         String[] encargados = new String[5];
         double[][] produccion = new double[5][12];
-        double[] totalProduccion = new double[5];
+        double[] totalProduccion = {1000, 3000, 1200, 1300, 2400};
 
         for (int i = 0; i < 5; i++) {
-            System.out.println("Ingrese nombre de la estación");
+            System.out.println("Ingrese nombre de la estacion " + (i + 1) +":");
             nombresEstaciones[i] = entrada.nextLine();
 
-            System.out.println("Ingrese nombre del encargado:");
+            System.out.println("Ingrese nombre del encargado por favor:");
             encargados[i] = entrada.nextLine();
 
             for (int j = 0; j < 12; j++) {
-                System.out.println("Producción del mes");
+                System.out.println("Produccion del mes " + (j + 1) + ":");
                 produccion[i][j] = entrada.nextDouble();
-                
             }
+            entrada.nextLine();
+        }
+        for (int i = 0; i < 5; i++) {
+            totalProduccion[i] = 0;
+            for (int j = 0; j < 12; j++) {
+                totalProduccion[i] = totalProduccion[i] + produccion[i][j];
             }
+        }
+        
+        double valor1 = totalProduccion[0];
+        int indiceM = 0;
+
+        for (int i = 1; i < 5; i++) {
+            if (totalProduccion[i] > valor1) {
+                valor1 = totalProduccion[i];
+                  i= indiceM;
+        
+
+        }
+        }
         }
     }
     
